cc01689e6b1ab7e0f4dbb123f2e46fc3169ef2f8
cancerhermit <cancerhermit@gmail.com>
2013-03-03 16:17:33 +0400

    "rm README.html"

--- delete-standalone-pyc.py
+++ delete-standalone-pyc.py
@@ -1,13 +1,13 @@
 from os.path import relpath
 from platform import python_version
 from sys import exc_info
-from sublime import active_window,error_message,packages_path
+from sublime import active_window, error_message,packages_path
 import sublime_plugin
 try:
     import dir
     import sublime_helper
 except Exception, e:
-    package = relpath(__file__,packages_path())
+    package = relpath(__file__, packages_path())
     line = exc_info()[2].tb_lineno
     error_message("""python%s\n%s
 line %s:\n
@@ -16,8 +16,9 @@ line %s:\n
     type(e), str(e)
     ))
 
+
 def unlink_pyc(dirname):
-    for file in dir.files(dirname,tree=True,filter="*.pyc"):
+    for file in dir.files(dirname, tree=True,filter="*.pyc"):
         dir.unlink(file)
 
 
@@ -26,7 +27,7 @@ class DeletePycListener(sublime_plugin.EventListener):
         try:
             for dirname in active_window().folders():
                 sublime_helper.thread(
-                    lambda:unlink_pyc(dirname),
+                    lambda: unlink_pyc(dirname),
                     status="search standalone .pyc"
                 )
         except Exception, e:
