3df4aa5349da7f0b29a688853b2250add41e4f5c
cancerhermit <cancerhermit@gmail.com>
2013-03-20 00:26:30 +0400

    "2013-03-20 00:26"

--- delete-standalone-pyc.py
+++ delete-standalone-pyc.py
@@ -7,20 +7,20 @@ import sublime_plugin
 try:
     import dir
     import sublime_helper
-    pkg =sublime_helper.package(__file__)
+    pkg = sublime_helper.package(__file__)
 except Exception, e:
     package = relpath(__file__, packages_path())
     line = exc_info()[2].tb_lineno
     error_message("""python%s\n%s:%s\n
 %s:\n%s""" % (
-    python_version(), package, line,
-    type(e), str(e)
-    ))
+                  python_version(), package, line,
+                  type(e), str(e)
+                  ))
 
 
 def unlink_pyc(dirname):
     for filepyc in dir.files(dirname, tree=True, filter="*.pyc"):
-        filepy =filepyc[:-1]
+        filepy = filepyc[:-1]
         if not dir.exists(filepy):
             dir.unlink(filepyc)
 
