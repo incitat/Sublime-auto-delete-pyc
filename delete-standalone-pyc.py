4ee1c347be687dfac0461a181e3c997ac7558a4a
cancerhermit <cancerhermit@gmail.com>
2013-03-19 23:58:42 +0400

    "2013-03-19 23:58"

--- delete-standalone-pyc.py
+++ delete-standalone-pyc.py
@@ -7,7 +7,7 @@ import sublime_plugin
 try:
     import dir
     import sublime_helper
-    pkg=sublime_helper.package(__file__)
+    pkg =sublime_helper.package(__file__)
 except Exception, e:
     package = relpath(__file__, packages_path())
     line = exc_info()[2].tb_lineno
@@ -17,11 +17,13 @@ except Exception, e:
     type(e), str(e)
     ))
 
+
 def unlink_pyc(dirname):
     for filepyc in dir.files(dirname, tree=True, filter="*.pyc"):
-        filepy=file[:-1]
+        filepy =filepyc[:-1]
         if not dir.exists(filepy):
             dir.unlink(filepyc)
+
 
 class DeletePycListener(sublime_plugin.EventListener):
     def run(self):
@@ -34,7 +36,7 @@ class DeletePycListener(sublime_plugin.EventListener):
     def on_pre_save(self, view):
         try:
             pkg.saferun(self.run)
-        except NameError: # pkg is undefined
+        except NameError:  # pkg is undefined
             pass
-        except Exception,e:
+        except Exception, e:
             error_message(str(e))
