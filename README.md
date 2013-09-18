a796f7c83925ab45b181ed5ee5f3a9fd6dad8139
eran <eran@incitat.com>
2013-09-18 18:49:34 +0300

    simplified. delete pyc after/post save of a .py file. does not delete all pyc in a project

--- README.md
+++ README.md
@@ -2,25 +2,3 @@ Features
 ----------
 
 +   delete .pyc files (on save)
-+   non blocking thread
-
-Platform
-----------
-**Windows**, **Mac OS**, **Linux**
-
-Requires
-----------
-
-    pip-2.6 install dir
-    pip-2.6 install sublime_helper 
-    # restart Sublime Text 2
-
-Installing
-----------
-
-Sublime package [Installation Instructions](https://cancerhermit.github.com/sublime-installation.html)
-
-Usage
-----------
-
-Autorun on save
--- delete-standalone-pyc.py
+++ delete-standalone-pyc.py
@@ -1,42 +1,18 @@
-#!/usr/bin/env python
-from os.path import relpath
-from platform import python_version
-from sys import exc_info
-from sublime import active_window, error_message, packages_path
+import sublime
 import sublime_plugin
-try:
-    import dir
-    import sublime_helper
-    pkg = sublime_helper.package(__file__)
-except Exception, e:
-    package = relpath(__file__, packages_path())
-    line = exc_info()[2].tb_lineno
-    error_message("""python%s\n%s:%s\n
-%s:\n%s""" % (
-                  python_version(), package, line,
-                  type(e), str(e)
-                  ))
-
-
-def unlink_pyc(dirname):
-    for filepyc in dir.files(dirname, tree=True, filter="*.pyc"):
-        filepy = filepyc[:-1]
-        if not dir.exists(filepy):
-            dir.unlink(filepyc)
+import os
 
 
 class DeletePycListener(sublime_plugin.EventListener):
-    def run(self):
-        for dirname in active_window().folders():
-            sublime_helper.thread(
-                lambda: unlink_pyc(dirname),
-                status="search standalone .pyc"
-            )
-
-    def on_pre_save(self, view):
+    def on_post_save(self, view):
         try:
-            pkg.saferun(self.run)
-        except NameError:  # pkg is undefined
+            f = view.file_name()
+            if f is not None:
+                f = os.path.abspath(f)
+                fpath, fext = os.path.splitext(f)
+                if fext == ".py":
+                    pyc_path = fpath + ".pyc"
+                    if os.path.exists(pyc_path):
+                        os.remove(pyc_path)
+        except Exception:
             pass
-        except Exception, e:
-            error_message(str(e))
